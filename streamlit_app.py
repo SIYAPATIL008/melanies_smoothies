49ac37818c8d6a02386ee336af541cc92176767b
SIYAPATIL008 <siya.patil@estuate.com>
2026-04-17 17:04:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,5 +54,5 @@ if ingredients_List:
 
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
     
