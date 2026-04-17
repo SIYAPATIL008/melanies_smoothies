d0c6491114f722fb2eba2c9ae9d663ed2339f55a
SIYAPATIL008 <siya.patil@estuate.com>
2026-04-17 12:57:04 +0530

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages.
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app.
@@ -21,6 +20,9 @@ ingredients_List = st.multiselect(
 
 #st.write(ingredients_List)
 #st.text(ingredients_List)
+
+cnx=st.connection("snowflake")
+session=cnx.session()
 
 if ingredients_List:
     ingredients_string=''
