270f5ffefdfdf7a87d449ee908e04a16c6613d48
SIYAPATIL008 <siya.patil@estuate.com>
2026-04-20 10:23:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write('The name on your Smoothies will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 smoothiefroot_response = requests.get(
-            "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
+            "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 st.stop()
